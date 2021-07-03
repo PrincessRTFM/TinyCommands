a4eef8576ff1878abdca1587a05853e77e526984
Lilith Song <lsong@princessrtfm.com>
2021-07-03 06:08:13 -0400

    Add a /nop command for waits in Macrology

---
+++ Commands/NoOp.cs
@@ -0,0 +1,16 @@
+﻿using Dalamud.Plugin;
+
+using TinyCmds.Attributes;
+
+namespace TinyCmds {
+	public partial class TinyCmdsPlugin: IDalamudPlugin {
+		[Command("/noop")]
+		[Summary("Does absolutely nothing. At all. Literally not a single thing.")]
+		[Aliases("/nop", "/null")]
+		[HelpMessage(
+			"This command does literally nothing at all.",
+			"Its only purpose is for use with Macrology, to allow macro-macros to use <wait.(delay)> without doing anything."
+		)]
+		public void EmptyCommand(string command, string[] args, FlagMap flags) { }
+	}
+}
