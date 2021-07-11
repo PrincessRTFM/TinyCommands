b589ce36443ddf44eebd1e37c131a77ab8f332ca
Lilith Song <lsong@princessrtfm.com>
2021-07-10 23:20:36 -0400

    Update the debug command (no touchy)

--- Commands/Debug.cs
+++ Commands/Debug.cs
@@ -10,12 +10,15 @@ namespace TinyCmds {
 		[DoNotShowInHelp]
 		[HideInCommandListing]
 		public void PluginDebugCommand(string command, string[] args, FlagMap flags) {
+			string jobName = this.Interface.ClientState.LocalPlayer.ClassJob.GameData.Abbreviation.ToString().ToUpper();
+			uint jobId = this.Interface.ClientState.LocalPlayer.ClassJob.Id;
 			this.Util.SendPrefixedChat(PluginUtil.Colour.GREY, "Received ", PluginUtil.Colour.BROWN, flags.Count, PluginUtil.Colour.GREY, " flags", PluginUtil.Colour.NONE);
 			this.Util.SendDirectChat(PluginUtil.Colour.INDIGO, string.Join(" ", flags.Keys), PluginUtil.Colour.NONE);
 			this.Util.SendPrefixedChat(PluginUtil.Colour.GREY, "Received ", PluginUtil.Colour.BROWN, args.Length, PluginUtil.Colour.GREY, " arguments", PluginUtil.Colour.NONE);
 			foreach (string arg in args) {
 				this.Util.SendDirectChat(PluginUtil.Colour.INDIGO, arg, PluginUtil.Colour.NONE);
 			}
+			this.Util.SendPrefixedChat($"Current job is {jobName} ({jobId})");
 		}
 	}
 }
