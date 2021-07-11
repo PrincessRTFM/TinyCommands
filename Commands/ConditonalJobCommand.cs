9b0e7a559f88682f0cb10e9b77d9c22e1c673d80
Lilith Song <lsong@princessrtfm.com>
2021-07-10 23:21:29 -0400

    Add /ifjob command

---
+++ Commands/ConditonalJobCommand.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+
+using Dalamud.Plugin;
+
+using TinyCmds.Attributes;
+
+namespace TinyCmds {
+	public partial class TinyCmdsPlugin: IDalamudPlugin {
+		[Command("/ifjob")]
+		[Arguments("'-n'?", "jobs to match against", "command to run...?")]
+		[RawArgs]
+		[Summary("Run a chat command (or directly send a message) only when playing certain classes/jobs")]
+		[Aliases("/ifclass", "/whenjob", "/whenclass")]
+		[HelpMessage(
+			"Much like /ifcmd and /ifgp, this command executes a given command when the condition is met.",
+			"In this case, the condition is whether or not your current class/job is one of the given set.",
+			"Use the three-letter abbreviation, and if you want to check against more than one, separate them with commas but NOT spaces.",
+			"If you pass the -n flag, the match will be inverted so the command runs only when you AREN'T one of those jobs."
+		)]
+		public void RunIfJobMatches(string command, string[] args, FlagMap flags) {
+			string arg = args[0] ?? string.Empty;
+			string currentJobName = this.Interface.ClientState.LocalPlayer.ClassJob.GameData.Abbreviation.ToString().ToUpper();
+			string wantedJobNames = arg.Split()[0].ToUpper();
+			string cmd = arg.Substring(wantedJobNames.Length).Trim();
+			bool invert = flags["n"];
+			bool match = wantedJobNames.Split(',').Contains(currentJobName);
+			if (match ^ invert) {
+				if (cmd.Length > 0) {
+					this.Util.SendServerChat(cmd);
+				}
+				else {
+					this.Util.SendPrefixedChat(
+						PluginUtil.Colour.GREEN,
+						"You are currently a ",
+						PluginUtil.Colour.PALEGREEN,
+						currentJobName,
+						PluginUtil.Colour.NONE
+					);
+				}
+			}
+			else if (cmd.Length < 1) {
+				this.Util.SendPrefixedChat(
+					PluginUtil.Colour.GREEN,
+					"You are currently a ",
+					PluginUtil.Colour.YELLOW,
+					currentJobName,
+					PluginUtil.Colour.NONE
+				);
+			}
+		}
+	}
+}
