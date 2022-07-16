03cb81485fc62a8e9017e16a37a1fa3bfc511247
Lilith Song <lsong@princessrtfm.com>
2022-07-15 23:38:00 -0400

    Add /clearflag to remove your map marker

---
+++ Commands/ClearMapFlag.cs
@@ -0,0 +1,18 @@
+﻿namespace TinyCmds;
+
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+
+using TinyCmds.Attributes;
+using TinyCmds.Utils;
+
+public static partial class PluginCommands {
+	[Command("/clearflag")]
+	[Summary("Remove the flag marker from your map")]
+	[Arguments()]
+	[HelpMessage(
+		"This command is a conveniently macro-able way to remove your map's flag marker."
+	)]
+	[Aliases("/unflag")]
+	public static unsafe void ClearMapFlag(string? command, string argline, FlagMap flags, ref bool showHelp)
+		=> AgentMap.Instance()->IsFlagMarkerSet = 0;
+}
