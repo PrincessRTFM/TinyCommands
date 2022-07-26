e941bcaeffd59aa7ceee44950f195ca622e4abcb
Lilith Song <lsong@princessrtfm.com>
2022-07-26 07:11:10 -0400

    Add game function hook constructor for IntPtr address

--- Internal/GameFunctionBase.cs
+++ Internal/GameFunctionBase.cs
@@ -30,6 +30,12 @@ internal abstract class GameFunctionBase<T> where T : Delegate {
 			Logger.warning($"{this.GetType().Name} FAILED, could not find address from signature: ${sig.ToUpper()}");
 		}
 	}
+	internal GameFunctionBase(IntPtr address, int offset = 0) {
+		this.addr = address + offset;
+		ulong totalOffset = (ulong)this.Address.ToInt64() - (ulong)Plugin.scanner.Module.BaseAddress.ToInt64();
+		Logger.info($"{this.GetType().Name} loaded; address = 0x{this.Address.ToInt64():X16}, base memory offset = 0x{totalOffset:X16}");
+	}
+
 	public dynamic? Invoke(params dynamic[] parameters)
 		=> this.Delegate?.DynamicInvoke(parameters);
 }
