a4551b155a665f070143978c7b801d977da79f03
Lilith Song <lsong@princessrtfm.com>
2022-08-20 19:01:41 -0400

    New custom placeholder mechanic for /eval

--- TinyCmds/Commands/EvaluatePlaceholders.cs
+++ TinyCmds/Commands/EvaluatePlaceholders.cs
@@ -1,7 +1,9 @@
 namespace TinyCmds.Commands;
 
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 using Dalamud.Memory;
 
@@ -22,10 +24,15 @@ using TinyCmds.Utils;
 	"If you use one of :@#$%^&*+ as a flag, only placeholders that start with that character will be replaced, as in <:target>.",
 	"This is to allow passing actual placeholders in the event that a command has only partial support.",
 	"All placeholders that identify players or NPCs are supported, as are <class>/<job>. No others are evaluated, due to a lack of consistent/sensible formatting.",
-	"There are a handful of non-vanilla placeholders that are also executed, which can only be used with this command.",
-	"They are: year, month, month0, day, day0,weekday, yearday, yearday0, hour/hour24, hour12, min/minute, sec/second, ms/milli/millisec/millisecond/milliseconds, am/pm.",
-	"Month, day of month, and day of year can take the suffix \"0\" to be zero-based.",
-	"These placeholders can be suffixed with \".utc\" to use UTC/GMT instead of local time. Due to internal restrictions, \"<am>\" is lowercase, while \"<pm>\" is uppercase.",
+	"There are a number of non-vanilla placeholders that are also executed, which can only be used with this command.",
+	"- classname/jobname work like <class>/<job> but only output your (full) class/job name, without level. The suffix \".short\" will output the abbreviated form.",
+	"- level/lvl will output only your level as a number, without anything else. The \".short\" suffix will disable zero-padding.",
+	"- year, month, day, weekday, yearday, hour, min/minute, sec/second, ms, and am/pm act as you would expect.",
+	"- month, day, and yearday can take \".0\" as a suffix to be zero-based (January/Sunday is 0, not 1) if desired.",
+	"- hour can take \".12\" as a suffix to use 12-hour time if desired. It can also take \".short\" to not zero-pad (6 -> 06) if desired.",
+	"All date/time placeholders can be suffixed with \".utc\" to use UTC/GMT instead of local time.",
+	"All placeholders that output text (instead of numbers) can take the suffixes \".upper\" and \".lower\" to specify their case.",
+	"Multiple suffixes can be used in any order, such that \"hour.12.short\" and \"hour.short.12\" will both work the same.",
 	"As with all commands that send input to the server, -? will display the command before it's run, and -! will display the command without running it."
 )]
 public class EvaluatePlaceholders: PluginCommand {
@@ -44,40 +51,58 @@ public class EvaluatePlaceholders: PluginCommand {
 	private static unsafe string readObjName(GameObject* obj)
 		=> obj is null ? "" : MemoryHelper.ReadStringNullTerminated(new IntPtr(obj->Name));
 
-	private static unsafe string replace(string p)
-		=> p.Trim().ToLower() switch {
-			// Custom placeholders - time - local
-			"year" => DateTime.Now.Year.ToString(),
-			"month" => (DateTime.Now.Month + 1).ToString("D2"),
-			"month0" => DateTime.Now.Month.ToString("D2"),
-			"day" => (DateTime.Now.Day + 1).ToString("D2"),
-			"day0" => DateTime.Now.Day.ToString("D2"),
-			"weekday" => DateTime.Now.DayOfWeek.ToString(),
-			"yearday" => (DateTime.Now.DayOfYear + 1).ToString("D3"),
-			"yearday0" => DateTime.Now.DayOfYear.ToString("D3"),
-			"hour" or "hour24" => DateTime.Now.Hour.ToString("D2"),
-			"hour12" => ((DateTime.Now.Hour % 12) + 1).ToString("D2"),
-			"min" or "minute" => DateTime.Now.Minute.ToString("D2"),
-			"sec" or "second" => DateTime.Now.Second.ToString("D2"),
-			"ms" or "milli" or "millisec" or "millisecond" or "milliseconds" => DateTime.Now.Millisecond.ToString("D3"),
-			"am" => DateTime.Now.Hour < 12 ? "am" : "pm",
-			"pm" => DateTime.Now.Hour < 12 ? "AM" : "PM",
-			// Custom placeholders - time - UTC
-			"year.utc" => DateTime.UtcNow.Year.ToString(),
-			"month.utc" => (DateTime.UtcNow.Month + 1).ToString("D2"),
-			"month0.utc" => DateTime.UtcNow.Month.ToString("D2"),
-			"day.utc" => (DateTime.UtcNow.Day + 1).ToString("D2"),
-			"day0.utc" => DateTime.UtcNow.Day.ToString("D2"),
-			"weekday.utc" => DateTime.UtcNow.DayOfWeek.ToString(),
-			"yearday.utc" => (DateTime.UtcNow.DayOfYear + 1).ToString("D3"),
-			"yearday0.utc" => DateTime.UtcNow.DayOfYear.ToString("D3"),
-			"hour.utc" or "hour24.utc" => DateTime.UtcNow.Hour.ToString("D2"),
-			"hour12.utc" => ((DateTime.UtcNow.Hour % 12) + 1).ToString("D2"),
-			"min.utc" or "minute.utc" => DateTime.UtcNow.Minute.ToString("D2"),
-			"sec.utc" or "second.utc" => DateTime.UtcNow.Second.ToString("D2"),
-			"ms.utc" or "milli.utc" or "millisec.utc" or "millisecond.utc" or "milliseconds.utc" => DateTime.UtcNow.Millisecond.ToString("D3"),
-			"am.utc" => DateTime.UtcNow.Hour < 12 ? "am" : "pm",
-			"pm.utc" => DateTime.UtcNow.Hour < 12 ? "AM" : "PM",
+	private static unsafe string replace(string p) {
+		string[] parts = p.ToLower().Split('.', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+		string flag = parts[0];
+		HashSet<string> mods = new(parts.Skip(1));
+		string value = flag.Trim().ToLower() switch {
+			// Custom placeholders - time
+			"year"
+				=> (mods.Contains("utc") ? DateTime.UtcNow : DateTime.Now).Year.ToString(),
+			"month"
+				=> (
+					(mods.Contains("utc") ? DateTime.UtcNow : DateTime.Now).Month
+					+ (mods.Contains("0") ? 0 : 1)
+				).ToString("D2"),
+			"day"
+				=> (
+					(mods.Contains("utc") ? DateTime.UtcNow : DateTime.Now).Day
+					+ (mods.Contains("0") ? 0 : 1)
+				).ToString("D2"),
+			"weekday"
+				=> (mods.Contains("utc") ? DateTime.UtcNow : DateTime.Now).DayOfWeek.ToString(),
+			"yearday"
+				=> (
+					(mods.Contains("utc") ? DateTime.UtcNow : DateTime.Now).DayOfYear
+					+ (mods.Contains("0") ? 0 : 1)
+				).ToString("D3"),
+			"hour"
+				=> (
+					(
+						(mods.Contains("utc") ? DateTime.UtcNow : DateTime.Now).Hour
+						% (mods.Contains("12") ? 12 : 24)
+					)
+					+ (mods.Contains("12") ? 1 : 0)
+				).ToString(mods.Contains("short") ? "D" : "D2"),
+			"min" or "minute"
+				=> (mods.Contains("utc") ? DateTime.UtcNow : DateTime.Now).Minute.ToString("D2"),
+			"sec" or "second"
+				=> (mods.Contains("utc") ? DateTime.UtcNow : DateTime.Now).Second.ToString("D2"),
+			"ms" or "milli" or "millisec" or "millisecond" or "milliseconds"
+				=> (mods.Contains("utc") ? DateTime.UtcNow : DateTime.Now).Millisecond.ToString("D3"),
+			"am" or "pm"
+				=> (mods.Contains("utc") ? DateTime.UtcNow : DateTime.Now).Hour < 12 ? "am" : "pm",
+			// Custom placeholders - personal stats
+			"classname" or "jobname"
+				=> Plugin.client.LocalPlayer is null
+					? ""
+					: mods.Contains("short")
+						? Plugin.client.LocalPlayer.ClassJob.GameData!.Abbreviation.ToString()
+						: Plugin.client.LocalPlayer.ClassJob.GameData!.Name.ToString(),
+			"level" or "lvl"
+				=> Plugin.client.LocalPlayer is null
+					? ""
+					: Plugin.client.LocalPlayer.Level.ToString(mods.Contains("short") ? "D" : "D2"),
 			// Vanilla placeholders - targets
 			"t" or "target"
 			or "tt" or "t2t"
@@ -95,12 +120,20 @@ public class EvaluatePlaceholders: PluginCommand {
 			or "ignore1" or "ignore2"
 			or "square" or "circle" or "cross" or "triangle"
 			or "mo" or "mouse"
-			or "e1" or "e2" or "e3" or "e4" or "e5" => readObjName(Framework.Instance()->UIModule->GetPronounModule()->ResolvePlaceholder($"<{p}>", 1, 0)),
+			or "e1" or "e2" or "e3" or "e4" or "e5"
+				=> readObjName(Framework.Instance()->UIModule->GetPronounModule()->ResolvePlaceholder($"<{p}>", 1, 0)),
 			// Vanilla placeholders - other
-			"class" or "job" => Plugin.client.LocalPlayer is null ? "" : Plugin.client.LocalPlayer.ClassJob.GameData!.Name.ToString() + "(" + Plugin.client.LocalPlayer.Level + ")",
+			"class" or "job"
+				=> Plugin.client.LocalPlayer is null ? "" : Plugin.client.LocalPlayer.ClassJob.GameData!.Name.ToString() + "(" + Plugin.client.LocalPlayer.Level + ")",
 			// Not a placeholder
 			_ => p,
 		};
+		return mods.Contains("upper")
+			? value.ToUpper()
+			: mods.Contains("lower")
+			? value.ToLower()
+			: value;
+	}
 
 	protected override unsafe void Execute(string? command, string rawArguments, FlagMap flags, bool verbose, bool dryRun, ref bool showHelp) {
 		string lead = "";
