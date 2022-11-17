7cfe6208743e45a969d0aa63d0cf961fec04a328
Lilith Song <lsong@princessrtfm.com>
2022-11-17 06:04:21 -0500

    New conditional /ifmount command

---
+++ TinyCmds/Commands/Conditional/ConditionalMount.cs
@@ -0,0 +1,84 @@
+﻿namespace PrincessRTFM.TinyCmds.Commands.Conditional;
+
+using System;
+using System.Linq;
+
+using Dalamud.Game.ClientState.Conditions;
+
+using PrincessRTFM.TinyCmds.Attributes;
+using PrincessRTFM.TinyCmds.Chat;
+using PrincessRTFM.TinyCmds.Utils;
+
+using PC = FFXIVClientStructs.FFXIV.Client.Game.Character.Character;
+
+[Command("/ifmount")]
+[Arguments("'-n'?", "mount IDs to match against", "command to run...?")]
+[Summary("Run a chat command (or directly send a message) only when using a specific mount")]
+[HelpMessage(
+	"This command's test is whether or not your current mount ID is one of the given set."
+	+ " Use the numeric ID, and if you want to check against more than one, separate them with commas but NOT spaces."
+	+ " If you pass the -n flag, the match will be inverted so the command runs only when you AREN'T using one of the given mounts."
+	+ " If you are not mounted, your effective mount ID is 0, which can be used for the check.",
+	"",
+	"Using -g will print your current mount ID, to make it easier to find the one you want."
+)]
+
+public class ConditionalMount: BaseConditionalCommand {
+	private const string currentMountLabel = "Your current mount ID is ";
+	protected override unsafe bool TryExecute(string? command, string args, FlagMap flags, bool verbose, bool dryRun, ref bool showHelp) {
+		string arg = args ?? string.Empty;
+		PC* player = (PC*)Plugin.client.LocalPlayer!.Address; // LocalPlayer is guaranteed to be non-null by BaseConditionalCommand
+		PC.MountContainer? mount = Plugin.conditions[ConditionFlag.Mounted] || Plugin.conditions[ConditionFlag.Mounted2] ? player->Mount : null;
+		ushort mountId = mount?.MountId ?? 0;
+
+		if (flags['g']) {
+			ChatUtil.ShowPrefixedMessage($"{currentMountLabel} {mountId}");
+			return false;
+		}
+
+		string wantedMountIds = arg.Split()[0];
+
+		if (string.IsNullOrEmpty(wantedMountIds)) {
+			showHelp = true;
+			return false;
+		}
+
+		string cmd = arg.Contains(' ')
+			? arg[(wantedMountIds.Length + 1)..]
+			: string.Empty;
+		bool invert = flags["n"];
+		bool match = wantedMountIds.Split(',', StringSplitOptions.RemoveEmptyEntries).Contains(mountId.ToString());
+
+		if (match ^ invert) {
+
+			if (cmd.Length > 0) {
+				ChatUtil.SendChatlineToServer(cmd, dryRun || verbose, dryRun);
+			}
+			else {
+				ChatUtil.ShowPrefixedMessage(
+					ChatColour.CONDITION_PASSED,
+					currentMountLabel,
+					ChatGlow.CONDITION_PASSED,
+					mountId,
+					ChatGlow.RESET,
+					ChatColour.RESET
+				);
+			}
+
+			return true;
+		}
+
+		if (cmd.Length < 1) {
+			ChatUtil.ShowPrefixedMessage(
+				ChatColour.CONDITION_FAILED,
+				currentMountLabel,
+				ChatGlow.CONDITION_FAILED,
+				mountId,
+				ChatGlow.RESET,
+				ChatColour.RESET
+			);
+		}
+
+		return false;
+	}
+}
