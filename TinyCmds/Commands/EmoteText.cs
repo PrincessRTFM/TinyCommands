d293618ce5f3cc40818a4939fb4856ab9477338b
Lilith Song <lsong@princessrtfm.com>
2023-01-24 19:43:07 -0500

    Add new /emotetext command
    Pass an emote command and then some custom emote text, and the emote
    will be run silently (without the action text) alongside the custom
    emote text in `/em` to simulate a custom emote macro in one command.
    
    For example, `/cem pat pets the cute <t>` will simultaneously run
    `/pat motion` and `/em pets the cute <t>` in a single line.
    
    This command was inspired by a request for similar functionality at
    https://github.com/Caraxi/SimpleTweaksPlugin/issues/431 for the
    SimpleTweaks plugin.

---
+++ TinyCmds/Commands/EmoteText.cs
@@ -0,0 +1,31 @@
+﻿namespace PrincessRTFM.TinyCmds.Commands;
+
+using PrincessRTFM.TinyCmds.Attributes;
+using PrincessRTFM.TinyCmds.Chat;
+using PrincessRTFM.TinyCmds.Utils;
+
+[Command("/emotetext")]
+[Summary("Perform an emote without text, and simultaneously perform a custom-text emote to go with it")]
+[Aliases("/emotion", "/customemote", "/cemote", "/cem", "/ce")]
+[HelpMessage(
+	"This command condenses the given emote command (with the \"motion\" argument to silence it) and the \"/em\" command to simulate a custom emote macro in one command.",
+	"For example, you can do \"/cem pat gives <t> some headpats\" to play the /pat animation with the custom text \"<Your Character> gives <Target Name> some headpats\" without needing a whole macro."
+)]
+public class EmoteText: PluginCommand {
+	protected override void Execute(string? command, string rawArguments, FlagMap flags, bool verbose, bool dryRun, ref bool showHelp) {
+		if (string.IsNullOrWhiteSpace(rawArguments) || !rawArguments.Contains(' ')) {
+			showHelp = true;
+			return;
+		}
+		string emoteName = rawArguments[..rawArguments.IndexOf(' ')].Replace("/", "").Trim();
+		string actionText = rawArguments[rawArguments.IndexOf(' ')..].Trim();
+		if (string.IsNullOrWhiteSpace(emoteName) || string.IsNullOrWhiteSpace(actionText)) {
+			showHelp = true;
+			return;
+		}
+		string emote = Plugin.common.Functions.Chat.SanitiseText($"/{emoteName} motion");
+		string action = Plugin.common.Functions.Chat.SanitiseText($"/em {actionText}");
+		ChatUtil.SendChatlineToServer(emote, verbose, dryRun);
+		ChatUtil.SendChatlineToServer(action, verbose, dryRun);
+	}
+}
