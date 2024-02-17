1f2b259d619dd596e0319e5a94407022df6b473b
Lilith Song <lsong@princessrtfm.com>
2024-02-17 00:10:25 -0500

    add new /all command

---
+++ TinyCmds/Commands/AllianceOrPartyChat.cs
@@ -0,0 +1,22 @@
+using PrincessRTFM.TinyCmds.Attributes;
+using PrincessRTFM.TinyCmds.Chat;
+using PrincessRTFM.TinyCmds.Utils;
+
+namespace PrincessRTFM.TinyCmds.Commands;
+
+[Command("/all", "/ap", "/pa", "/duty")]
+[Arguments("message to send...?")]
+[Summary("Acts as either /alliance or /party, depending on whether you're in an alliance")]
+[HelpText(
+	"This command checks whether you are currently in an alliance in order to determine whether to simulate /alliance or /party.",
+	"If any additional (non-flag) arguments are provided, they are passed through directly to the chosen command.",
+	"This allows you to use this command to send a single message, as well as to change your active chat channel."
+)]
+public class AllianceOrPartyChat: PluginCommand {
+	protected override void Execute(string? command, string rawArguments, FlagMap flags, bool verbose, bool dryRun, ref bool showHelp) {
+		if (Plugin.Party.IsAlliance)
+			ChatUtil.SendChatlineToServer($"/alliance {rawArguments}".Trim(), verbose, dryRun);
+		else
+			ChatUtil.SendChatlineToServer($"/party {rawArguments}".Trim(), verbose, dryRun);
+	}
+}
