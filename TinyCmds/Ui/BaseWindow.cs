871d8aa1d1af9bd481cc18b33b478c1a8da6b945
Lilith Song <lsong@princessrtfm.com>
2024-02-23 05:08:02 -0500

    add github repo button

--- TinyCmds/Ui/BaseWindow.cs
+++ TinyCmds/Ui/BaseWindow.cs
@@ -13,19 +13,29 @@ internal abstract class BaseWindow: Window {
 	protected BaseWindow(string name, ImGuiWindowFlags flags = ImGuiWindowFlags.None) : base(name, WindowFlags | flags) {
 		this.RespectCloseHotkey = true;
 		this.IsOpen = false;
-		TitleBarButton kofi = new() {
-			Priority = int.MinValue,
-			Icon = FontAwesomeIcon.Heart,
-			IconOffset = new(2, 1),
-			Click = _ => Process.Start(new ProcessStartInfo("https://ko-fi.com/V7V7IK9UU") { UseShellExecute = true }),
-			ShowTooltip = () => {
-				ImGui.BeginTooltip();
-				ImGui.TextUnformatted("Support me on ko-fi");
-				ImGui.EndTooltip();
-			},
-		};
 		this.TitleBarButtons = new() {
-			kofi,
+			new() {
+				Priority = 0,
+				Icon = FontAwesomeIcon.Heart,
+				IconOffset = new(2, 1),
+				Click = _ => Process.Start(new ProcessStartInfo("https://ko-fi.com/V7V7IK9UU") { UseShellExecute = true }),
+				ShowTooltip = () => {
+					ImGui.BeginTooltip();
+					ImGui.TextUnformatted("Support me on ko-fi");
+					ImGui.EndTooltip();
+				},
+			},
+			new() {
+				Priority = 1,
+				Icon = FontAwesomeIcon.Code,
+				IconOffset = new(1, 1),
+				Click = _ => Process.Start(new ProcessStartInfo("https://github.com/PrincessRTFM/TinyCommands") { UseShellExecute = true }),
+				ShowTooltip = () => {
+					ImGui.BeginTooltip();
+					ImGui.TextUnformatted("Browse the github repo");
+					ImGui.EndTooltip();
+				},
+			},
 		};
 		this.AllowClickthrough = true;
 		this.AllowPinning = true;
